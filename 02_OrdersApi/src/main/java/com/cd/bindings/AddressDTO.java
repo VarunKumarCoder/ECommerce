fb92b0be2bab0f52736c8349d4fa96c8b220842e
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-04 19:59:59 +0530

    mapping class addes

--- 02_OrdersApi/src/main/java/com/cd/bindings/AddressDTO.java
+++ 02_OrdersApi/src/main/java/com/cd/bindings/AddressDTO.java
@@ -1,5 +1,20 @@
 package com.cd.bindings;
 
-public class AddressDTO {
+import com.cd.entity.Customer;
 
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+public class AddressDTO {
+	
+	private String houseno;
+	private String street;
+	private String city;
+	private String state;
+	private String zipcode;
+	private String country;
+	
+	@ManyToOne
+	private Customer customer;
 }
