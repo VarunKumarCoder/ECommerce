61097fd7fbd0f2fa5f4bc3fa47baec13e57485a0
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-04 16:38:00 +0530

    feat - ProductCategory entity class added

--- 01_ProductsApi/src/main/java/com/cd/entity/ProductCategory.java
+++ 01_ProductsApi/src/main/java/com/cd/entity/ProductCategory.java
@@ -1,5 +1,26 @@
 package com.cd.entity;
 
-public class ProductCategory {
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="product_category")
+@Data
+public class ProductCategory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String categoryName;
+	
+	@OneToMany(mappedBy = "category",cascade = CascadeType.ALL)
+	private Set<Product> products;
 }
