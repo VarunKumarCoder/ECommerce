ac4d072ca3b2348bc594c40b7f9b87b4a50e085e
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-04 20:12:44 +0530

    Order binding class completed

--- 02_OrdersApi/src/main/java/com/cd/bindings/OrderDto.java
+++ 02_OrdersApi/src/main/java/com/cd/bindings/OrderDto.java
@@ -1,6 +1,30 @@
 package com.cd.bindings;
 
+import java.util.List;
 
+import com.cd.entity.Address;
+import com.cd.entity.Customer;
+import com.cd.entity.OrderItem;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
 public class OrderDto {
-
+	
+	private int totalquantity;
+	private double totalprice;
+	private String razorPayOrderId;
+	private String orderStatus;
+	private String razorPayPaymentId;
+	
+	@ManyToOne
+	private Customer customer;
+	
+	@ManyToOne
+	private Address address;
+	
+	@ElementCollection
+	private List<OrderItem> orderItems;
 }
