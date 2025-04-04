332adba42e48c0b3d2ad79178db6eac92c4650bb
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-04 20:42:19 +0530

    application.properties added

--- 02_OrdersApi/src/main/java/com/cd/service/RazorPayService.java
+++ 02_OrdersApi/src/main/java/com/cd/service/RazorPayService.java
@@ -1,11 +1,27 @@
 package com.cd.service;
 
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.cd.config.RazorPayConfig;
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+
+
 
 @Service
 public class RazorPayService {
+	@Autowired
+	private RazorpayClient razorPayClient;
+	
+	public String createQrCodeOrder(int amount) throws RazorpayException {
+        JSONObject orderRequest = new JSONObject();
+        orderRequest.put("amount", amount * 100); // amount in paise
+        orderRequest.put("currency", "INR");
+        orderRequest.put("payment_capture", 1);
 
-	private RazorPayClient 
+        Order order = razorPayClient.Orders.create(orderRequest);
+        return order.get("id");
+    }
 }
