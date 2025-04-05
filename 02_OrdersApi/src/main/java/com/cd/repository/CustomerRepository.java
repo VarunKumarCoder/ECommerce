cf07656c527e25773cd95e5b48bb5459214dffd4
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-05 17:51:57 +0530

    Customer Repository created

---
+++ 02_OrdersApi/src/main/java/com/cd/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.cd.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cd.bindings.CustomerDTO;
+import com.cd.entity.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerDTO, Long>{
+
+	public Customer findByEmail(String Email);
+	
+}
