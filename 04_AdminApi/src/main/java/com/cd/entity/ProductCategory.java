879c1c7a5b555013e41e771d78d501c8bf3bbab1
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-08 01:22:26 +0530

    Product repository created

--- 04_AdminApi/src/main/java/com/cd/entity/ProductCategory.java
+++ 04_AdminApi/src/main/java/com/cd/entity/ProductCategory.java
@@ -1,5 +1,19 @@
 package com.cd.entity;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="product_category")
 public class ProductCategory {
 
+	private Long id;
+	
+	private String categoryName;
+	@OneToMany(mappedBy="category",cascade = CascadeType.ALL)
+	private Set<Product> products;
 }
