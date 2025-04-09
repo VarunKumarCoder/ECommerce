cfda8adae1135e6a594d41a650739dcc12c3e1cb
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-09 19:06:33 +0530

    Spring Context class created

---
+++ 05_NotificationsApi/src/main/java/com/cd/listner/SpringContext.java
@@ -0,0 +1,15 @@
+package com.cd.listner;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class SpringContext implements ApplicationContextAware {
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
