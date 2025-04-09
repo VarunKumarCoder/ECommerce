d7172b73a59ab1673bf93b854be830bcbe8da1b2
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-09 15:59:45 +0530

    whatsapp notification service method created

--- 05_NotificationsApi/src/main/java/com/cd/service/WatiService.java
+++ 05_NotificationsApi/src/main/java/com/cd/service/WatiService.java
@@ -6,4 +6,6 @@ public interface WatiService {
 
 	public WatiResponse sendDeliveryNotification(String phno, String name, String OrderTrackingNumber);
 	
+	public WatiResponse sendPaymentRemainder(String phno, String name, String orderTrackingNumber);
+
 }
